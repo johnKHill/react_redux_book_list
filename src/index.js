e8a9751272a870346ca6e2bde156cf87354c2842
Johknotter Hill <johknotterhill@gmail.com>
2018-08-17 02:14:52 -0400

    Added link to redux store to access the redux dev tools

--- src/index.js
+++ src/index.js
@@ -6,10 +6,16 @@ import { createStore, applyMiddleware } from 'redux';
 import App from './components/app';
 import reducers from './reducers';
 
+
 const createStoreWithMiddleware = applyMiddleware()(createStore);
+// This is for React Dev Tools
+const store = createStoreWithMiddleware(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>
   , document.querySelector('.container'));
+
+
+
